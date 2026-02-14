7dda2e158d52655e2485999aec5a787c94d690d7
Peizhe Tian <peizhe2001@hotmail.com>
2026-02-14 14:53:58 -0500

    add gaussian function

--- lib/pong/quantumConstant.ts
+++ lib/pong/quantumConstant.ts
@@ -3,3 +3,14 @@ export const HBAR = PLANCK_CONSTANT / (2 * Math.PI);
 export const SIGMA_0 = 15; // initial uncertainty
 export const SIGMA_MAX = 90; // maximum uncertainty
 export const TIME_SCALE = 2e5; // used to convert frame into time value that makes the formula work
+
+export function gaussianRandom(): number {
+  let u, v, s;
+  do {
+    u = Math.random() * 2 - 1;
+    v = Math.random() * 2 - 1;
+    s = u * u + v * v;
+  } while (s >= 1 || s === 0);
+  const multiplier = Math.sqrt((-2 * Math.log(s)) / s);
+  return u * multiplier;
+}
